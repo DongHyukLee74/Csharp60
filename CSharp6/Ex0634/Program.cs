b91e32c3d4997d0a9f69a67b44bd817e40a6239b
peter <peter@PETER-NOTEBOOK>
2016-06-11 00:41:26 +0900

    20160610

--- CSharp6/Ex0634/Program.cs
+++ CSharp6/Ex0634/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Ex0634
 {
@@ -10,6 +9,46 @@ namespace Ex0634
     {
         static void Main(string[] args)
         {
+            // 서버 소켓이 동작하는 스레드
+            Thread serverThread = new Thread(serverFunc);
+            serverThread.IsBackground = true;
+            serverThread.Start();
+            Thread.Sleep(500);  // 소켓 서버용 스레드가 실행될 시간을 주기 위해
+
+            Console.WriteLine("종료하려면 아무 키나 누르세요...");
+            Console.ReadLine();
+        }
+
+        private static void serverFunc(object obj)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //IPAddress ipAddress = GetCurrentIPAddress();
+
+            //if (ipAddress == null)
+            //{
+            //    Console.WriteLine("IPv4용 랜 카드가 없습니다.");
+            //    return;
+            //}
+
+            //IPEndPoint endPoint = new IPEndPoint(ipAddress, 10200);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 10200);
+            socket.Bind(endPoint);
+        }
+
+        // 현재 컴퓨터에 장착된 네트워크 어댑터의 IPv4 주소를 반환
+        private static IPAddress GetCurrentIPAddress()
+        {
+            IPAddress[] addrs = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+
+            foreach (IPAddress ipAddr in addrs)
+            {
+                if (ipAddr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ipAddr;
+                }
+            }
+
+            return null;
         }
     }
 }
---
+++ CSharp6/Ex0635/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Ex0635
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 서버 소켓이 동작하는 스레드
+            Thread serverThread = new Thread(serverFunc);
+            serverThread.IsBackground = true;
+            serverThread.Start();
+            Thread.Sleep(500);  // 소켓 서버용 스레드가 실행할 시간을 주기 위해
+
+            Console.WriteLine("종료하려면 아무 키나 누르세요...");
+            Console.ReadLine();
+        }
+
+        private static void serverFunc(object obj)
+        {
+            using (Socket srvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                IPAddress ipAddress = IPAddress.Parse("0.0.0.0");
+                IPEndPoint endPoint = new IPEndPoint(ipAddress, 10200);
+
+                srvSocket.Bind(endPoint);
+
+                byte[] recvBytes = new byte[1024];
+                EndPoint clientEP = new IPEndPoint(IPAddress.None, 0);
+
+                while (true)
+                {
+                    int nRecv = srvSocket.ReceiveFrom(recvBytes, ref clientEP);
+                    string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+
+                    byte[] sendBytes = Encoding.UTF8.GetBytes("Hello: " + txt);
+                    srvSocket.SendTo(sendBytes, clientEP);
+                }
+            }
+        }
+    }
+}
