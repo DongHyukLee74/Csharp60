a1d3dcb19109cbdc18318fbfcadb34334097d105
peter <peter@PETER-NOTEBOOK>
2016-06-18 22:12:40 +0900

    20160618

--- CSharp6/ClassLibrary3/ClassLibrary3.cs
+++ CSharp6/ClassLibrary3/ClassLibrary3.cs
@@ -3,7 +3,29 @@ using System.Collections.Generic;
 
 namespace ClassLibrary3
 {
-    public class Class1
+    [PluginAttribute]
+    public class SystemInfo
+    {
+        bool _is64Bit;
+
+        public SystemInfo()
+        {
+            _is64Bit = Environment.Is64BitOperatingSystem;
+            Console.WriteLine("SystemInfo created.");
+        }
+
+        [StartupAttribute]
+        public void WriteInfo()
+        {
+            Console.WriteLine("OS == {0}bits", (_is64Bit == true) ? "64" : "32");
+        }
+    }
+
+    public class PluginAttribute : Attribute
+    {
+    }
+
+    public class StartupAttribute : Attribute
     {
     }
 }
---
+++ CSharp6/Ex0665/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Ex0665
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            BigInteger int1 = BigInteger.Parse("12345678901234567890");
+            BigInteger int2 = BigInteger.Parse("98765432109876543210");
+
+            Console.WriteLine(int1 + int2); // 출력 결과: 11111111101111111100
+
+            BigInteger int3 = 9223372036854775807;
+            */
+            Console.WriteLine(IntPtr.Size);
+            // 출력 결과
+            // 32비트 프로그램인 경우: 4
+            // 64비트 프로그램인 경우: 8
+
+            using (FileStream fs = new FileStream("test.bat", FileMode.Create))
+            {
+                Console.WriteLine(fs.Handle);
+            }
+        }
+    }
+}
