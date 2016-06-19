1e32aaebd965b702263f2da438e86c94395b86c4
peter <peter@PETER-NOTEBOOK>
2016-06-19 23:18:26 +0900

    20160619

--- CSharp6/Ex0665/Program.cs
+++ CSharp6/Ex0665/Program.cs
@@ -2,6 +2,8 @@
 using System.Numerics;
 using Microsoft.Win32;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Ex0665
 {
@@ -9,6 +11,7 @@ namespace Ex0665
     {
         static void Main(string[] args)
         {
+            #region 571
             /*
             BigInteger int1 = BigInteger.Parse("12345678901234567890");
             BigInteger int2 = BigInteger.Parse("98765432109876543210");
@@ -16,7 +19,7 @@ namespace Ex0665
             Console.WriteLine(int1 + int2); // 출력 결과: 11111111101111111100
 
             BigInteger int3 = 9223372036854775807;
-            */
+            
             Console.WriteLine(IntPtr.Size);
             // 출력 결과
             // 32비트 프로그램인 경우: 4
@@ -25,6 +28,20 @@ namespace Ex0665
             using (FileStream fs = new FileStream("test.bat", FileMode.Create))
             {
                 Console.WriteLine(fs.Handle);
+            }*/
+            #endregion 571
+            // 제네릭이 없던 시절
+            {
+                int n = 5;
+                ArrayList ar = new ArrayList();
+                ar.Add(n);
+            }
+
+            // 제네릭이 구현된 C# 2.0 이후
+            {
+                int n = 5;
+                List<int> list = new List<int>();
+                list.Add(n);
             }
         }
     }
---
+++ CSharp6/Ex0701/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ex0701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
